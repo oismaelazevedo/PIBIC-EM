65d5f94aec60a09f82c00d117f4e091a36b93324
oismaelazevedo <62020604+oismaelazevedo@users.noreply.github.com>
2021-02-01 16:42:43 -0300

    Update GeradorQuest163.py
    Gerando arquivos JSON.

--- src/json/GeradorQuest163.py
+++ src/json/GeradorQuest163.py
@@ -1,49 +1,108 @@
 import random as rnd
 import json
+import math
 
-expoenteConcentracao = rnd.randint(1,70)
-resposta = round((expoenteConcentracao ** 2)/3, 2)
 
-listLetra = ["A","B","C","D","E"]
-questaoCerta = rnd.choice(listLetra)
-questaoInvertida = rnd.choice(listLetra)
 
-# Verifica se a letra com a resposta certa e invertida são iguais 
-repetida = True
-while repetida == True: 
-    if questaoCerta == questaoInvertida:
-        questaoCerta = rnd.choice(listLetra)
-        questaoInvertida = rnd.choice(listLetra)
-    else: repetida = False
- 
-listAlternativas = [0,0,0,0,0]
+for k in range(200):
 
-# Insere a resposta certa na letra escolhida para ser certa, uma letra recebe a questão invertida e o resto recebe números aleatórios
-for numLetra in range(0, 5):
-    if questaoCerta == listLetra[numLetra]:
-        listAlternativas[numLetra] = resposta
-    elif questaoInvertida == listLetra[numLetra]:
-        listAlternativas[numLetra] = round(-(expoenteConcentracao ** 3)/2, 2)
-    else:
-        EhNegativoPositivo = rnd.randint(0,1)
-        if (EhNegativoPositivo == 0): 
-            listAlternativas[numLetra] = round((rnd.randint(1,expoenteConcentracao) ** 2)/3, 2) 
-        else: 
-            listAlternativas[numLetra] = round(-(rnd.randint(1,expoenteConcentracao) ** 2)/3, 2)
+    questoes = open('questao{}-163.json'.format(k+1), 'w')
 
-# Dá duas tentativas para o indivíduo acertar
-for numTentativa in range(2):
-    print(f"O pH de uma solução é definido por pH = log(1/H+), em que H+ é a concentração de hidrogênio em íons-gama por litro de solução. Determine o quadrado sobre três do pH de uma solução tal que H+ = 1,0 x e{expoenteConcentracao}. Admita que e = 10")
-    print(f"a) {listAlternativas[0]}")
-    print(f"b) {listAlternativas[1]}")
-    print(f"c) {listAlternativas[2]}")
-    print(f"d) {listAlternativas[3]}")
-    print(f"e) {listAlternativas[4]}")
-    respAluno = input()
+    expoenteConcentracao = rnd.randint(1,70)
+    resposta = round((expoenteConcentracao ** 2)/3, 2)
 
-    if(respAluno.upper() == questaoCerta):
-        print("Parabéns!!! Acertou miserável!!!")
-        break
-    else:
-        print("Resposta errada. Tente novamente")
+    listLetra = ["A","B","C","D","E"]
+    questaoCerta = rnd.choice(listLetra)
+    questaoInvertida = rnd.choice(listLetra)
 
+    # Verifica se a letra com a resposta certa e invertida são iguais 
+    repetida = True
+    while repetida == True: 
+        if questaoCerta == questaoInvertida:
+            questaoCerta = rnd.choice(listLetra)
+            questaoInvertida = rnd.choice(listLetra)
+        else: repetida = False
+    
+    listAlternativas = [0,0,0,0,0]
+    isCorrect = ['','','','','']
+    howGenerated = ['','','','','']
+
+    # Insere a resposta certa na letra escolhida para ser certa, uma letra recebe a questão invertida e o resto recebe números aleatórios
+    for numLetra in range(0, 5):
+        if questaoCerta == listLetra[numLetra]:
+            listAlternativas[numLetra] = resposta
+            isCorrect[numLetra] = "Sim"
+            howGenerated[numLetra] = "nenhum"
+        elif questaoInvertida == listLetra[numLetra]:
+            listAlternativas[numLetra] = round(-(expoenteConcentracao ** 3)/2, 2)
+            isCorrect[numLetra] = "Nao"
+            howGenerated[numLetra] = "invertida"
+        else:
+            EhNegativoPositivo = rnd.randint(0,1)
+            if (EhNegativoPositivo == 0): 
+                listAlternativas[numLetra] = round((rnd.randint(1,expoenteConcentracao) ** 2)/3, 2) 
+                isCorrect[numLetra] = "Nao"
+                howGenerated[numLetra] = "gerada aleatoriamente e positiva"
+            else: 
+                listAlternativas[numLetra] = round(-(rnd.randint(1,expoenteConcentracao) ** 2)/3, 2)
+                isCorrect[numLetra] = "Nao"
+                howGenerated[numLetra] = "gerada aleatoriamente e negativa"
+
+
+    # Cria a variável que será convertida em um arquivo json
+    dados = {
+        'logaritmo' : [
+            {
+                'base' : 10,
+                'logaritmando' : 1 / 10 ** expoenteConcentracao,
+                'resultado' : round(math.log(1 / 10 ** expoenteConcentracao, 10),2)    
+            }
+        ],
+        'respostas' : [
+            {
+                'letra': 'a)',
+                'resposta': listAlternativas[0],
+                'correta': isCorrect[0],
+                'tipoerro': howGenerated[0]
+            },
+            {
+                'letra': 'b)',
+                'resposta': listAlternativas[1],
+                'correta': isCorrect[1],
+                'tipoerro': howGenerated[1]
+            },
+            {
+                'letra': 'c)',
+                'resposta': listAlternativas[2],
+                'correta': isCorrect[2],
+                'tipoerro': howGenerated[2]
+            },{
+                'letra': 'd)',
+                'resposta': listAlternativas[3],
+                'correta': isCorrect[3],
+                'tipoerro': howGenerated[3]
+            },{
+                'letra': 'e)',
+                'resposta': listAlternativas[4],
+                'correta': isCorrect[4],
+                'tipoerro': howGenerated[4]
+            }
+        ],
+        'atributosquestao': [
+            {
+                'enunciado': 'O pH de uma solução é definido por pH = log(1/H+), em que H+ é a concentração de hidrogênio em íons-gama por litro de solução. Determine o quadrado sobre três do pH de uma solução tal que H+ = 1,0 x e{}. Admita que e = 10'.format(expoenteConcentracao),
+                'corretaspossiveis': listAlternativas[isCorrect.index("Sim")],
+                'corretas': isCorrect.count("Sim"),
+                'aleatoriapositiva': howGenerated.count("gerada aleatoriamente e positiva"),
+                'aleatorianegativa': howGenerated.count("gerada aleatoriamente e negativa"),
+                'invertida': howGenerated.count("invertida"),
+                'respostascorretas': listLetra[isCorrect.index("Sim")]
+            }
+        ]
+    }
+
+    # Cria o arquivo JSON
+    print("\nquestao {}\n".format(k+1),json.dumps(dados))
+    json.dump(dados, questoes, indent=4)
+
+questoes.close()
