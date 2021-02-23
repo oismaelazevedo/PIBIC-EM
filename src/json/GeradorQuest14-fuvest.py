8d6f8f09539e3f47d45adc4121a104baad09947c
oismaelazevedo <62020604+oismaelazevedo@users.noreply.github.com>
2021-02-23 00:42:45 -0300

    Adicionando a funcionalidade de não geração de enunciados iguais

--- src/json/GeradorQuest14-fuvest.py
+++ src/json/GeradorQuest14-fuvest.py
@@ -141,6 +141,7 @@ while k < 200:
     # Cria o arquivo JSON
         print("\nquestao {}\n".format(k+1),json.dumps(dados))
         json.dump(dados, questoes, indent=4)
+        
         k = k + 1
 
 questoes.close()
