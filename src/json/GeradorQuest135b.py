a9abf297068ac1258642df3c8b04bb22a8687820
oismaelazevedo <62020604+oismaelazevedo@users.noreply.github.com>
2021-02-23 23:45:44 -0300

    Adicionando a funcionalidade de não geração de enunciados iguais

--- src/json/GeradorQuest135b.py
+++ src/json/GeradorQuest135b.py
@@ -151,6 +151,7 @@ while k < 100:
     # Cria o arquivo JSON
         print("\nquestao {}\n".format(k+1),json.dumps(dados))
         json.dump(dados, questoes, indent=4)
+        
         k = k + 1
 
 
