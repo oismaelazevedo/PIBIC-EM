432e0b01c4595d62e28824253bb9bc87c7826685
oismaelazevedo <62020604+oismaelazevedo@users.noreply.github.com>
2021-02-26 14:29:40 -0300

    Adicionando a funcionalidade de não geração de enunciados iguais

--- src/json/GeradorQuest135j.py
+++ src/json/GeradorQuest135j.py
@@ -15,7 +15,7 @@ enunciado = [None]*100
 k = 0
 while k < 100:
 
-    questoes = open("questoes{}-135j.json".format(k+1),'w')
+    questoes = open("questao{}-135j.json".format(k+1),'w')
 
     numBaseLogaritmo = rnd.randint(2,10)
     numExpoenteBase = rnd.randint(-10,-1)
