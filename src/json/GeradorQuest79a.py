55588c555308ea15c20229a33872d222e9cb757f
oismaelazevedo <62020604+oismaelazevedo@users.noreply.github.com>
2021-02-26 14:40:18 -0300

    Adicionando a funcionalidade de não geração de enunciados iguais

--- src/json/GeradorQuest79a.py
+++ src/json/GeradorQuest79a.py
@@ -10,9 +10,11 @@ def elementosListaEhDistinta(lista):
             elif lista[indiceLista] == lista[indiceListaComparacao] and indiceLista != indiceListaComparacao:
                 return False
 
-for k in range(50):
-
-    questoes = open("questoes{}-79a.json".format(k+1),'w')
+enunciado = [None]*50
+k = 0
+while k < 50:
+    
+    questoes = open("questao{}-79a.json".format(k+1),'w')
 
     numBase = rnd.randint(2,20)
     numExpoenteNumBase = rnd.randint(1,10)
@@ -124,11 +126,18 @@ for k in range(50):
         ]
     }
 
-    print('Resolva a seguinte equação exponencial:\n{} = {}'.format(pretty(express), numMultiplicadorIncognitaAlternativa * (numBase ** numExpoenteNumBase)))
-
+    # Verifica os enunciados
+    if dados['atributosquestao'][0]['enunciado'] in enunciado:
+        continue
+    else:
+        
+    # Armazena os enunciados
+        enunciado[k] = dados['atributosquestao'][0]['enunciado']
+        
     # Cria o arquivo JSON
-    print("\nquestao {}\n".format(k+1),json.dumps(dados))
-    json.dump(dados, questoes, indent=4)
+        print("\nquestao {}\n".format(k+1),json.dumps(dados))
+        json.dump(dados, questoes, indent=4)
+        k = k + 1
 
 questoes.close()
                 
