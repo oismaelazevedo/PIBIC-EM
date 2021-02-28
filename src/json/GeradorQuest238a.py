643b5b7de163c57ea0cc6fc8c50f1dac5b198df1
Estevão Naval <navalestevao@gmail.com>
2021-02-28 20:48:23 -0300

    Adicionando geradores 238 - letra A e C e gerador 401.

---
+++ src/json/GeradorQuest238a.py
@@ -0,0 +1,157 @@
+import random as rnd
+from sympy import Symbol
+import json
+
+
+def elementosListaEhDistinta(lista):
+    for indiceLista in range(len(lista)):
+        for indiceListaComparacao in range(len(lista)):
+            if indiceLista == 4:
+                return True
+            elif lista[indiceLista] == lista[indiceListaComparacao] and indiceLista != indiceListaComparacao:
+                return False
+
+enunciado = [None]*100
+k = 0
+while k < 100:
+
+    questoes = open("questao{}-238a.json".format(k+1), 'w')
+
+    numBaseLogaritmo = rnd.randint(2,1000)
+
+    valX1 = rnd.randint(2,100)
+    valX2 = rnd.randint(2,100)
+
+    valNum01 = rnd.randint(2,1000)
+    valNum02 = rnd.randint(2,1000)
+
+    while(valX1 == valX2 or valNum01 == valNum02):
+        valX1 = rnd.randint(2,100)
+        valX2 = rnd.randint(2,100)
+
+        valNum01 = rnd.randint(2,1000)
+        valNum02 = rnd.randint(2,1000)
+        
+
+    resposta = round((valNum02 - valNum01)/(valX1 - valX2), 2) 
+
+    listLetra = ["A","B","C","D","E"]
+
+    questaoCerta = rnd.choice(listLetra)
+    questaoInvertida = rnd.choice(listLetra)
+
+    while(questaoCerta == questaoInvertida):
+        questaoInvertida = rnd.choice(listLetra)
+        
+    listAlternativas = [0,0,0,0,0]
+    isCorrect = ['','','','','']
+    howGenerated = ['','','','','']
+
+    # Insere a resposta certa na letra escolhida para ser certa, uma letra recebe a questão invertida e o resto recebe números aleatórios
+    while(elementosListaEhDistinta(listAlternativas) == False):
+        possuiQuestaoInvertida = rnd.randint(0,1)
+
+        for numLetra in range(0,5):
+            if questaoCerta == listLetra[numLetra]:
+                listAlternativas[numLetra] = resposta
+                isCorrect[numLetra] = "Sim"
+                howGenerated[numLetra] = "nenhum"
+            elif questaoInvertida == listLetra[numLetra] and possuiQuestaoInvertida == 1:
+                
+                numRandomTemporario = rnd.randint(0,1)
+
+                if numRandomTemporario == 0:
+
+                    listAlternativas[numLetra] = round((valX2 - valX1)/(valNum01 - valNum02), 2) 
+                    isCorrect[numLetra] = "Nao"
+                    howGenerated[numLetra] = "invertida e positiva"
+                else:
+                    listAlternativas[numLetra] = -round((valX2 - valX1)/(valNum01 - valNum02), 2) 
+                    isCorrect[numLetra] = "Nao"
+                    howGenerated[numLetra] = "invertida e negativa"
+            else:
+                numRandomTemporario = rnd.randint(0,1)
+
+                if numRandomTemporario == 0:
+
+                    listAlternativas[numLetra] = round((rnd.randint(2,1000) - rnd.randint(2,1000))/rnd.randint(2,100),2)
+                    isCorrect[numLetra] = "Nao"
+                    howGenerated[numLetra] = "gerada aleatoriamente e positiva"
+                else:
+
+                    listAlternativas[numLetra] = -round((rnd.randint(2,1000) - rnd.randint(2,1000))/rnd.randint(2,100),2)
+                    isCorrect[numLetra] = "Nao"
+                    howGenerated[numLetra] = "gerada aleatoriamente e negativa"
+
+    # Cria a variável que será convertida em um arquivo json
+    dados = {
+        'logaritmo' : [
+            {
+                'numBaseLogaritmo': numBaseLogaritmo, 
+                'valX1': valX1,
+                'valX2': valX2,
+                'valNum01': valNum01,
+                'valNum02': valNum02,
+                'resposta': resposta,
+            }
+        ],
+        'respostas': [
+            {
+                'letra': 'a)',
+                'resposta': listAlternativas[0],
+                'correta': isCorrect[0],
+                'tipoerro': howGenerated[0]
+            },
+            {
+                'letra': 'b)',
+                'resposta': listAlternativas[1],
+                'correta': isCorrect[1],
+                'tipoerro': howGenerated[1]
+            },
+            {
+                'letra': 'c)',
+                'resposta': listAlternativas[2],
+                'correta': isCorrect[2],
+                'tipoerro': howGenerated[2]
+            },{
+                'letra': 'd)',
+                'resposta': listAlternativas[3],
+                'correta': isCorrect[3],
+                'tipoerro': howGenerated[3]
+            },{
+                'letra': 'e)',
+                'resposta': listAlternativas[4],
+                'correta': isCorrect[4],
+                'tipoerro': howGenerated[4]
+            }
+        ],
+        'atributosquestao': [
+            {
+                'enunciado': 'Resolva a equação: a) log{} ({}x + {}) = log{} ({}x + {})'.format(numBaseLogaritmo,valX1,valNum01,numBaseLogaritmo,valX2,valNum02),
+                'corretaspossiveis': listAlternativas[isCorrect.index("Sim")],
+                'corretas': isCorrect.count("Sim"),
+                'aleatoriapositiva': howGenerated.count("gerada aleatoriamente e positiva"),
+                'aleatorianegativa': howGenerated.count("gerada aleatoriamente e negativa"),
+                'invertidapositiva': howGenerated.count("invertida e positiva"),
+                'invertidanegativa': howGenerated.count("invertida e negativa"),
+                'respostascorretas': listLetra[isCorrect.index("Sim")]
+            }
+        ]
+    }
+
+
+    # Verifica os enunciados
+    if dados['atributosquestao'][0]['enunciado'] in enunciado:
+        continue
+    else:
+        print('Resolva a equação: a) log{} ({}x + {}) = log{} ({}x + {})'.format(numBaseLogaritmo,valX1,valNum01,numBaseLogaritmo,valX2,valNum02))
+        
+    # Armazena os enunciados
+        enunciado[k] = dados['atributosquestao'][0]['enunciado']
+        
+    # Cria o arquivo JSON
+        print("\nquestao {}\n".format(k+1),json.dumps(dados))
+        json.dump(dados, questoes, indent=4)
+        k = k + 1
+
+questoes.close()
